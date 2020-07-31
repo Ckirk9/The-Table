539a272487d1129e10aaafd49d3b73ae8f6f7fc3
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-07-31 18:35:42 -0400

    created new.ejs for new sessions and wrote route to new sessions in sessions.js

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,5 +6,9 @@ router.get('/', (req, res) => {
     res.render('sessions/index.ejs')
 }) 
 
+// path to sessions new
+router.get('/new', (req, res) => {
+    res.render('sessions/new.ejs')
+})
 
 module.exports = router
