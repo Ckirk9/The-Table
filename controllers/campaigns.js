5589417afda60dc4d0a5cbf9aca0526947b930f0
DylanGygax <dylangygax@ncf.edu>
2020-08-02 14:20:22 -0500

    campaigns: index.ejs and index route added

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -1,8 +1,21 @@
 // Require Statements
 const express = require('express')
 const router = express.Router()
+const Campaign = require('../models/campaign')
 //const methodOverride = require('method-override')
 
+
+// Routes
+
+// INDEX
+router.get('/', (req, res) => {
+    Campaign.find({}, (err,foundCampaigns) => (
+        //console.log(foundCampaigns)
+        res.render('campaigns/index.ejs', {
+            campaigns: foundCampaigns
+        })
+    ))
+})
 
 
 
