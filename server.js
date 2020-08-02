de2a252455151d32c1db51a6bdf1393240b10574
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-08-02 13:18:25 -0400

    connected express to DB got succesful connection

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 // require statements
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const sessionsController = require('./controllers/sessions.js')
 const campaignsController = require('./controllers/campaigns.js')
 
+//DB connection
+const connectionString = 'mongodb://localhost/the-table'
+const db = mongoose.connection
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+db.on('connected', () => console.log(`Mongoose connected to ${connectionString}`))
+db.on('disconnected', () => console.log('Mongoose disconnected'))
+db.on('error', (err) => console.log('Mongoose error', err))
+
 //middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+// route for sessions
 app.use('/sessions', sessionsController)
 
-
-
-// route for campaign
+// route for campaigns
 app.use('/campaigns', campaignsController)
 
 // root route to homepage
