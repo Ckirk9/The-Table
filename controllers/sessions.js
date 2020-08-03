b6ba55dfa405ef6e3cd2ef288bceb5f336fb406e
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-08-03 19:42:30 -0400

    refactored create route to clean up unused code and comments

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -45,32 +45,6 @@ router.get('/new/:id', (req, res) => {
     })
 })
 
-
-// // SHOW
-// router.get('/:id', (req,res) => {
-//     Campaign.findById(req.params.id) 
-//     .populate({path: 'sessions'}) //maybe need to add "match"
-//     .exec((err, foundCampaign) => {
-//         //Session.find({}, (err, foundSessions) => {
-//         if (err) {console.log(err)}
-//         res.render('campaigns/show.ejs', {
-//             campaign: foundCampaign,
-//             //sessions: foundSessions
-//         })
-//     })
-// })
-
-// path to show page
-// router.get('/:id', (req, res) => {
-//     Session.findById(req.params.id)
-//     .populate({path: 'campaign'})
-//     .exec((err, foundSession) => {
-//         if (err) {console.log(err)}
-//         res.render('sessions/show.ejs', {
-//             session: foundSession
-//         })
-//     })
-// })
 router.get('/:id', (req, res) => {
     Session.findById(req.params.id, req.body, (err, foundSession) => {
         Campaign.findById(foundSession.campaign, (err, foundCampaign) => {
@@ -81,28 +55,15 @@ router.get('/:id', (req, res) => {
         })
     })
 })
+
 // create route
-// router.post('/:id', (req, res) => {
-//     Session.create(req.body, (err, createdSession) => {
-//         Campaign.findById(req.params.id, (err, foundCampaign) => {
-//             req.body.body = "pizza"
-//             req.body.campaign = req.params.id
-//             foundCampaign.sessions.push(createdSession._id)
-//             foundCampaign.save((err, savedCampaign) => {
-//                 res.redirect('/campaigns/' + req.params.id)
-//             })
-//         })
-//     })
-// })
+
 router.post('/:id', async (req, res) => {
     try {
-        //req.body.body = "pizza"
         req.body.campaign = req.params.id
         const createdSession = await Session.create(req.body)
-        //console.log(createdSession)
         const foundCampaign = await Campaign.findById(req.params.id)
         await foundCampaign.sessions.push(createdSession._id)
-        //console.log(foundCampaign)
         foundCampaign.save((err, savedCampaign) => {
             res.redirect('/campaigns/' + req.params.id)
         })
