0f69b8a722e33cc28392b193ffca3d1538c5e33f
DylanGygax <dylangygax@ncf.edu>
2020-08-03 12:26:10 -0500

    editted ejs files to be more desciptive and removed duplicate code.

--- models/campaign.js
+++ models/campaign.js
@@ -6,7 +6,7 @@ const campaignSchema = mongoose.Schema ({
     dungeonMaster: {type: String, required: true },
     players: {type: String, required: false },
     openSlots: {type: Number, required: true },
-    startDate: {type: String, required: true }, // Look into using a Date for this!
+    startDate: {type: Date, required: true }, // Look into using a Date for this!
     // Sessions: [{type: _id, required: false },]
 })
 
