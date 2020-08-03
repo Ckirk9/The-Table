42e0bb2a9bf0e4d73382b25ee3c3238978010c36
DylanGygax <dylangygax@ncf.edu>
2020-08-03 12:38:07 -0500

    added sessions array to campaign model.

--- models/campaign.js
+++ models/campaign.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
+const Session = require('./session')
 
-const campaignSchema = mongoose.Schema ({
+const campaignSchema = new mongoose.Schema ({
     name: {type: String, required: true },
     description: {type: String, required: true },
     dungeonMaster: {type: String, required: true },
     players: {type: String, required: false },
     openSlots: {type: Number, required: true },
-    startDate: {type: Date, required: true }, // Look into using a Date for this!
-    // Sessions: [{type: _id, required: false },]
+    startDate: {type: Date, required: true }, 
+    sessions: [{type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Session',
+        required: false
+    }]
 })
 
 const Campaign = mongoose.model('Campaign', campaignSchema)
